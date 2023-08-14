68bb1f4eebfd1bc25831d05e13b1a313a707303c
devfresher <fresher.dev01@gmail.com>
2023-08-14 09:56:46 +0100

    Blog & Typography correction

--- src/components/landingUi/ourBlog.jsx
+++ src/components/landingUi/ourBlog.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import vector from '../../assets/png/Vector 1.png'
-import stage from '../../assets/png/stage.png'
-import {BsFillArrowRightCircleFill} from 'react-icons/bs'
+import Blog from "../composable/blog";
+const blogs = [
+  {date: "25th June 2023", title: "Singing and Songwriting"},
+  {date: "18th May 2023", title: "Stand-Up Comedy"},
+  {date: "17th May 2023", title: "Painting and Visual Arts"},
+]
 const OurBlog = () => {
   return (
     <div className="w-full  bg-gray-100 py-6 sm:py-8 space-y-5 sm:space-y-8">
@@ -15,26 +19,9 @@ const OurBlog = () => {
         <img src={vector} alt='' className="mx-auto" />
       </div>
       <div className="grid grid-cols-1 w-full items-center px-4 md:flex sm:px-20 gap-6 lg:gap-10 ">
-       {[1,2,3].map((i,j) => {
+       {blogs.map((blog, index) => {
         return (
-            <div
-            key={j}
-            className="w-full rounded-xl max-[640px]:h-[350px] h-[350px] max-[890px]:h-[300px]">
-            <div className="w-full max-[640px]:h-[250px] h-[250px] max-[890px]:h-[200px] rounded-t-xl overflow-hidden">
-                <img src={stage} alt="" className="w-full h-full rounded-t-xl" />
-            </div>
-            <div className="bg-gray-200 rounded-b-xl p-3">
-                <div className="flex self-end justify-end items-end w-full">
-                    <BsFillArrowRightCircleFill className="text-[25px] text-black"/>
-                </div>
-                <div>
-                    <p className="text-[10px] sm:text-xs text-[#017297]">26th june</p>
-                    <p className="font-semibold text-lg sm:text-xl">Lorem Ipsum</p>
-                </div>
-
-            </div>
-
-        </div>
+          <Blog key={index} title={blog.title} date={blog.date}/>
         )
        })}
       </div>
