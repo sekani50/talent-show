eed2838f4f647d4064925575cb88ab81e67e678f
sekani50 <mohammedadeoye0@gmail.com>
2023-10-19 17:38:41 -0700

    update

--- src/components/landingUi/ongoing.jsx
+++ src/components/landingUi/ongoing.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import {motion} from "framer-motion";
 import { toast } from "react-hot-toast";
 import axios from "../../Utils/useAxios";
+import { formatDate } from "../../Utils/stringtoDate";
 const Ongoing = () => {
   const [data, setdata] = useState([]);
   const { token } = useSelector((state) => state.user);
@@ -75,6 +76,21 @@ const Ongoing = () => {
         Ongoing Events
       </h1>
 
+      <div className="text-white  mx-auto space-y-4 flex flex-col items-center w-fit h-fit ">
+          <p className="font-bold text-lg sm:text-3xl">{data?.eventName || ''}</p>
+          <p className="text-center">A show brought to you by NextGen</p>
+          <button
+            onClick={() => {
+              joinEvent(data._id, data?.eventName, data?.categories)
+            }}
+            className="text-[#017297] bg-white rounded-sm px-4 sm:px-8 py-2"
+          >
+            Join as a Contestant
+          </button>
+          <p className="text-center text-[#FFCC15]">
+          {`  Registration Ends by ${formatDate(data?.contestEnd).month || ''} ${formatDate(data?.contestEnd).year || ''}`}
+          </p>
+        </div>
      
     </div>
   );
